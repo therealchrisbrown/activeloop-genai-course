904441d8da6f2e647246b51a7fae1ec6820aa178
therealchrisbrown <ch.braun1@gmx.de>
2023-11-10 08:46:59 +0100

    add question to perplexity

--- perplexity.py
+++ perplexity.py
@@ -6,3 +6,6 @@ sentence_probabilities_normalized = sentence_probabilities ** (1/len(probabiliti
 perplexity = 1 / sentence_probabilities_normalized
 
 print(perplexity)
+
+# Suppose we further train the LLM, and the probabilities of the next best word become higher. How would the final perplexity be, higher or lower?
+# Would become lower
