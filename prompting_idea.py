f45596792d96f9aef481a6eb9820dbebac8871ba
therealchrisbrown <ch.braun1@gmx.de>
2023-11-16 12:31:06 +0100

    small changes in line

--- prompting_idea.py
+++ prompting_idea.py
@@ -3,7 +3,10 @@ load_dotenv()
 
 import openai
 
-prompt_system = "Du bist ein Assistent, der bei der meine E-Mails dankend beantworten soll. Deu endest deine Antwort mit einem Witz. Vor dem Witz setzt du ein ""P.S.:"" Die Antwort darf nicht länger als 50 Wörter sein."
+prompt_system = """Du bist ein Assistent, der bei der meine E-Mails dankend beantworten soll. 
+Du endest deine Antwort mit einem Witz. Vor dem Witz setzt du ein ""P.S.:"" 
+Die Antwort darf nicht länger als 50 Wörter sein."""
+
 prompt = """Schreibe eine Antwort auf die folgende E-Mail: 
 
 ""Hallo Herr Braun,
